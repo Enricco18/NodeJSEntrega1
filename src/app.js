50fc9e3b79d4b5f5c15a5aae32b942969f37f20f
Enricco Diniz <enriccodiniz@pop-os.localdomain>
2020-05-19 15:58:14 -0300

    Commit apos realizacao dos testes |19/05 16h

--- src/app.js
+++ src/app.js
@@ -1,33 +1,84 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+// { id: "uuid", 
+//title: 'Desafio Node.js', 
+//url: 'http://github.com/...', 
+//techs: ["Node.js", "..."], 
+//likes: 0 }
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+  repositories.push({
+    id:uuid(),
+    title,
+    url,
+    techs,
+    likes:0
+  })
+  return response.json(repositories);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title,url,techs} = request.body;
+  const index = repositories.findIndex((item)=>{
+    return item.id == id;
+  })
+
+  if(index<0){
+    return response.status(400).json({error:"Not found"})
+  }
+
+  repositories[index] = {
+    id : repositories[index].id,
+    title: title?title:repositories[index].title,
+    url: url?url:repositories[index].url,
+    techs: techs?techs:repositories[index].techs,
+    likes: repositories[index].likes
+  }
+
+  return response.json(repositories);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const index = repositories.findIndex((item)=>{
+    return item.id == id;
+  })
+  if(index<0){
+    return response.status(400).json({error:"Not found"})
+  }
+
+  repositories.splice(index,1)
+  return response.json(repositories);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const index = repositories.findIndex((item)=>{
+    return item.id == id;
+  })
+  if(index<0){
+    return response.status(400).json({error:"Not found"})
+  }
+
+  repositories[index].likes +=1
+
+  return response.json(repositories);
 });
 
 module.exports = app;
